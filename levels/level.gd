53daba9ed34d455c6de6df4259c38b022bba2a11
Mastermori <mastermoriii@gmail.com>
2025-01-25 13:50:43 +0100

    added level reset with "r"

--- levels/level.gd
+++ levels/level.gd
@@ -3,6 +3,11 @@ extends Node3D
 
 @onready var obstacles: Node3D = $Obstacles
 @onready var bubbles: Node3D = $Bubbles
+@onready var initial_player_position: Vector3 = $Player.position
 
 func _ready() -> void:
 	Globals.current_level = self
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("reset"):
+		Globals.player.position = initial_player_position
