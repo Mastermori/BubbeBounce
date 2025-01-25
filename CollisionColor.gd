b5cac404c1b2ed1c6873395370a67a7494236444
Joe <jimgrolle@gmx.de>
2025-01-25 09:43:56 +0100

    Experimentmenting with collision

--- CollisionColor.gd
+++ CollisionColor.gd
@@ -5,3 +5,14 @@ extends RigidBody3D
 func _on_body_entered(body):
 	var material = mesh.get_active_material(0)
 	material.albedo_color = Color(randf(), randf(), randf())
+	apply_impulse(-linear_velocity)
+	print("Colllision")
+
+# Probably not supposed to be here
+func _activate_grativitation(gravStrengh: float):
+	gravity_scale = gravStrengh
+
+# Probably not supposed to be here
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		_activate_grativitation(0.1 )
